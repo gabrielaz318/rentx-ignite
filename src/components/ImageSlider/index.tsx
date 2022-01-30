8f21622edc059b3f161cb1550a20fe3f2b289ad1
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2022-01-30 13:29:50 -0300

    ImageSlider finalizado

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
     Container,
@@ -12,22 +13,41 @@ interface Props {
     imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+    viewableItems: ViewToken[];
+    change: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+    const [imageIndex, setIndexImage] = useState(0);
+
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        setIndexImage(info.viewableItems[0].index!)
+    });
+
     return (
         <Container>
             <ImageIndexes>
-                <ImageIndex active={true} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
+                {imagesUrl.map((_, index) => <ImageIndex active={imageIndex === index} key={index} />)}
             </ImageIndexes>
 
-            <CarImageWrapper>
-                <CarImage
-                    source={{uri: imagesUrl[0] }}
-                    resizeMode='contain'
+            
+                <FlatList 
+                    data={imagesUrl}
+                    keyExtractor={item => item}
+                    renderItem={({item}) => (
+                        <CarImageWrapper>
+                        <CarImage
+                            source={{uri: item }}
+                            resizeMode='contain'
+                        />
+                        </CarImageWrapper>
+                    )}
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}
+                    onViewableItemsChanged={indexChanged.current}
                 />
-            </CarImageWrapper>
         </Container>
     );
 }
